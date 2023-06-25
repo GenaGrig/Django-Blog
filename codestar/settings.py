3b966e887e22a2407d121afc61a056933d78d157
GenaGrig <gennadiuss@gmail.com>
2023-06-25 10:54:14 +0000

    changes in settings and procfile for deployment

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['my-blog-project-codestar.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['my-blog-project-codestar-5b71b2e6baf4.herokuapp.com', 'localhost']
 
 
 # Application definition
